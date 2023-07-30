1b624c2ada3dadb6fafe3f57d8eeb2c9f3fb25c5
nam.phong thieu <phong.thieu@aegona.com>
2023-07-30 13:12:32 +0700

    update binh luan

--- src/binh_luan/binh_luan.controller.ts
+++ src/binh_luan/binh_luan.controller.ts
@@ -1,7 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, UseGuards,Get, Res } from '@nestjs/common';
 import { BinhLuanService } from './binh_luan.service';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { AuthGuard } from '@nestjs/passport';
 
+@ApiTags('Dat Phong')
+@ApiBearerAuth()
+@UseGuards(AuthGuard('jwt'))
 @Controller('binh-luan')
 export class BinhLuanController {
   constructor(private readonly binhLuanService: BinhLuanService) {}
+  @Get()
+  getBookedRoom(@Res() res: Response) {
+    return this.binhLuanService.getBinhLuan(res);
+  }
 }
--- src/binh_luan/binh_luan.service.ts
+++ src/binh_luan/binh_luan.service.ts
@@ -1,4 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+import { successCode } from 'src/config/response';
 
 @Injectable()
-export class BinhLuanService {}
+export class BinhLuanService {
+  prisma = new PrismaClient();
+  async getBinhLuan(res: Response) {
+    try {
+      let data = this.prisma.datPhong.findMany();
+      return successCode(res, data, '', 200);
+    } catch (err) {
+      throw new HttpException(err.response, err.status);
+    }
+  }
+}
